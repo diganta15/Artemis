0650d276581c24a702a81191ddbdf42e65187288
diganta15 <ddas461598@gmail.com>
2021-11-15 00:46:32 +0530

    Added testing library for frontend

--- artemis-frontend/.next/static/chunks/webpack.js
+++ artemis-frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3aa020bc214373b1"; }
+/******/ 		__webpack_require__.h = function() { return "43a32627bbeaf14a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
