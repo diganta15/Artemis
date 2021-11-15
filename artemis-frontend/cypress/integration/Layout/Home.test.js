b7a9ac38806874452b7f931dddacefec8f705182
diganta15 <ddas461598@gmail.com>
2021-11-16 01:02:16 +0530

    Made some changes to test

--- artemis-frontend/cypress/integration/Layout/Home.test.js
+++ artemis-frontend/cypress/integration/Layout/Home.test.js
@@ -1,7 +1,15 @@
-describe('Header', () => {
-    it('Should check if the site name is present',()=>{
-        cy.visit('http://localhost:3000')
+describe("Header", () => {
+	it("Should check if the site name and login button is present", () => {
+		cy.visit("http://localhost:3000");
 
-        cy.get('h1').contains('Artemis');
+		cy.get("h1").contains("Artemis");
+        cy.get("a").contains("Login");
+	});
+
+
+    it("Should check if the login button is working",()=>{
+        cy.visit("http://localhost:3000");
+        cy.get("a").contains("Login").click();
     })
-})
+});
+
